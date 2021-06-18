fe62929b6a40d40213e22b4ccac6f6b3931f1dfe
Edward Pavett <jettytjs@gmail.com>
2021-06-18 02:56:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+if (window.location.href.includes("index")) { document.getElementById("homeButton").className="topButton current"}
+if (window.location.href.includes("contact")) { document.getElementById("contactButton").className="topButton current"}
+if (window.location.href.includes("help")) { document.getElementById("helpButton").className="topButton current"}
+if (window.location.href.includes("login")) { document.getElementById("loginButton").className="topButton current"}
+if (window.location.href.includes("register")) { document.getElementById("registerButton").className="topButton current"}
